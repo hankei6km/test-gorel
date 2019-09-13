01db299f8ae93755e3a77c791459c72a7ab5695b
hankei6km <hankei6km@gmail.com>
2019-09-14 00:31:27 +0900

    Add a testing for foo function

---
+++ my_cmd/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_foo(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "basic",
+			args: args{s: "test"},
+			want: "test-gorel: test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := foo(tt.args.s)
+			assert.Equal(t, tt.want, got, "foo()")
+		})
+	}
+}
